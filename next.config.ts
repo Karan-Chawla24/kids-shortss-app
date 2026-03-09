05a1d83383490d904de4fbb8be0e5d15801c698d
KaranChawla <chawlakaran663@gmail.com>
2026-03-09 18:32:30 +0530

    fixed autoplay issue for prod version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value:
+              "autoplay=*, encrypted-media=*, fullscreen=*, picture-in-picture=*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
